6a9a31fbcce4b4f28f61458fa50b55d35399e465
Marianna Monteiro de Oliveira - 820120673 <820120673@ulife.com.br>
2022-08-29 21:36:12 -0300

    previsao do tempo Itu

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('dotenv').config()
 const axios = require('axios')
 const appid = process.env.appid
 
-const q = 'São Paulo'
+const q = 'Itu'
 //metric: celsius
 //imperial: farenheit
 const units = 'metric'
 const lang = 'pt_BR'
 const cnt = 10
 const base_url = 'https://api.openweathermap.org/data/2.5/forecast'
-const url = `${base_url}?q=${q}&${units}&lanf=${lang}&cnt=${cnt}&appid=${appid}`
+const url = `${base_url}?q=${q}&units=${units}&lang=${lang}&cnt=${cnt}&appid=${appid}`
 console.log(url)
+
+const p = axios.get(url)
+p.then((res) => console.log(res.data))
+.catch(err=> console.log(err))
